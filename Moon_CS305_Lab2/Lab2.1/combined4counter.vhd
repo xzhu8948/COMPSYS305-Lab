551ae9822ed215bcf961573acacc281b9552f823
xzhu8948 <xzhu664@aucklanduni.ac.nz>
2024-03-21 15:01:26 +1300

    Lab 2.1 finished and has already checked (Lab 1 has already checked on time on Tuesday beginning)

--- Moon_CS305_Lab2/Lab2.1/combined4counter.vhd
+++ Moon_CS305_Lab2/Lab2.1/combined4counter.vhd
@@ -1,72 +1,90 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+
 entity combined4counter is
   port (Clk, Reset, Enable : in bit;
-        Mode : in bit_vector (1 downto 0);
-        Q : out bit_vector (3 downto 0));
+        Mode : in std_logic_vector (1 downto 0);
+        Q : out std_logic_vector (3 downto 0));
 end entity combined4counter;
 
 architecture behaviour1 of combined4counter is
+	signal Q1: std_logic_vector (3 downto 0);
 begin
-  process (Clk)
-    variable v_Q: integer;
+  process (Clk, Reset)
   begin
-    
-    if Clk'event and Clk = '1' then
-      
+
+-- Reset from the begining
+    if (Reset'event and Reset='1') then
+	   if Mode = "00" then
+		    Q1 <= "1011";
+	   elsif Mode = "01" then
+		    Q1 <= "1010";
+	   elsif Mode = "10" then
+		    Q1 <= "0001";
+	   elsif Mode = "10" then
+		    Q1 <= "1111";
+	   end if;
+
+    elsif (Clk'event and Clk='1') then
+
+-- Start counter
+-- Mode "00"
       if Mode = "00" then
-        
-        if Enable = '0' then
-          v_Q := v_Q;
-        elsif Reset = '1' then
-          v_Q := '11';
-        elsif v_Q > '1' and v_Q <= '11' then
-          v_Q := v_Q - '1';
-        else
-          v_Q := '11';
-        end if;
-        
+        if Enable = '1' then
+        	if Q1 > "0001" and Q1 <= "1011" then
+          		Q1 <= Q1 - "0010";
+        	else
+          		Q1 <= "1011";
+        	end if;
+	      elsif Enable = '0' then
+		        Q1 <= Q1;
+	      end if;
+
+-- Mode "01"
       elsif Mode = "01" then
-        
-        if Enable = '0' then
-          v_Q := v_Q;
-        elsif Reset = '1' then
-          v_Q := '10';
-        elsif v_Q = '10' then
-          v_Q := '5';
-        elsif v_Q = '5' then
-          v_Q := '8';
-        elsif v_Q = '8' then
-          v_Q := '3';
-        elsif v_Q = '3' then
-          v_Q := '7';
-        elsif v_Q = '7' then
-          v_Q := '6';
-        elsif v_Q = '6' then
-          v_Q := '12';
-        else
-          v_Q := '10';
-        end if;
-      
+        if Enable = '1' then
+		     if Q1 = "1010" then
+          		Q1 <= "0101";
+        	elsif Q1 = "0101" then
+          		Q1 <= "1000";
+        	elsif Q1 = "1000" then
+          		Q1 <= "0011";
+        	elsif Q1 = "0011" then
+          		Q1 <= "0111";
+        	elsif Q1 = "0111" then
+          		Q1 <= "0110";
+        	elsif Q1 = "0110" then
+          		Q1 <= "1100";
+        	else
+          		Q1 <= "1010";
+        	end if;
+	      elsif Enable = '0' then
+		        Q1 <= Q1;
+	      end if;
+
+-- Mode "10"
       elsif Mode = "10" then
+        if Enable = '1' then
+        	if Q1 < "1101" and Q1 >= "0001" then
+          		Q1 <= Q1 + "0001";
+        	else
+          		Q1 <= "0001";
+        	end if;
+	      elsif Enable = '0' then
+		        Q1 <= Q1;
+	      end if;
         
-        if Enable = '0' then
-          v_Q := v_Q;
-        elsif Reset = '1' then
-          v_Q := '1';
-        elsif v_Q < '13' and v_Q >= '1' then
-          v_Q := v_Q + '1';
-        else
-          v_Q := '1';
-        end if;
-        
+-- Mode "11"
       elsif Mode = "11" then
         
-        v_Q := "1111";
+        Q1 <= "1111";
         
       end if;
       
-    end if;
+     end if;
     
-    Q <= v_Q;
-    
-  end process;
+   end process;
+
+   Q <= Q1;
 end architecture behaviour1;
--- Moon_CS305_Lab2/Lab2.1/test_counter.vhd
+++ Moon_CS305_Lab2/Lab2.1/test_counter.vhd
@@ -1,26 +1,30 @@
 -- Testbench for the counter. 
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+
 entity test_counter is
 end entity test_counter;
 
 architecture my_test of test_counter is 
     signal t_clk, t_reset, t_enable : bit;
-    signal t_mode : bit_vector (1 downto 0);
-    signal t_Q : bit_vector (3 downto 0);
+    signal t_mode : std_logic_vector (1 downto 0);
+    signal t_Q : std_logic_vector (3 downto 0);
 
     component combined4counter is 
         port (Clk, Reset, Enable : in bit;
-              Mode : in bit_vector (1 downto 0);
-              Q : out bit_vector (3 downto 0));
+              Mode : in std_logic_vector (1 downto 0);
+              Q : out std_logic_vector (3 downto 0));
     end component combined4counter;
 begin
      DUT: combined4counter port map (t_clk, t_reset, t_enable, t_mode, t_Q);
  
      -- Initialization process (code that executes only once).
-     init: process
+     init: process (t_mode)
      begin 
        -- reset pulse
-       t_reset <= '0', '1' after 2 ns, '0' after 7 ns;
-         wait;
+       t_reset <= '1', '0' after 1 ns;
      end process init;
 
      -- clock generation
@@ -36,19 +40,20 @@ begin
      enable_set: process
      begin
       t_enable <= '1';
+      wait;
      end process enable_set;
      
      -- mode set
      mode_set: process
      begin
       t_mode <= "00";
-      wait for 10 ns;
+      wait for 100 ns;
       t_mode <= "01";
-      wait for 10 ns;
+      wait for 100 ns;
       t_mode <= "10";
-      wait for 10 ns;
+      wait for 100 ns;
       t_mode <= "11";
-      wait for 10 ns;
+      wait for 100 ns;
      end process mode_set;
      
 end architecture my_test;
